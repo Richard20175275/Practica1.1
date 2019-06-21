b9b16d3d97c5f3ce7d1937c3af2139cc53948f81
Richa <Richa@DESKTOP-397FMC3>
2019-06-20 23:21:14 -0400

    Modifique el nombre del proyecto y la solucion, era WEB-API y lo cambie a WEBAPI y agregue una clase para el ejemplo.

---
+++ WEB-API/ClaseCambio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEBAPI
+{
+    public class ClaseCambio
+    {
+        public int MiPropiedad { get; set; }
+    }
+}
